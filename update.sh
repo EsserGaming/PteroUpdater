831278058613d5391695340a480811510623bff4
Esser <upwordstudent@gmail.com>
2021-05-02 12:53:53 -0500

    remove color

--- update.sh
+++ update.sh
@@ -1,12 +1,7 @@
-# Color info
-BLUE='\033[1;34m'
-LIGHT_GREEN='\033[1;32m'
-NoColor='\033[0m'
-
 # Panel
-echo "$LIGHT_GREEN" Updating panel
+echo Updating panel
 bash <(curl -s https://raw.githubusercontent.com/EsserGaming/PteroUpdater/main/panel.sh)
 
 # Wings
-echo "$BLUE" Updating wings
+echo Updating wings
 bash <(curl -s https://raw.githubusercontent.com/EsserGaming/PteroUpdater/main/wings.sh)
