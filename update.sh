b4e811cc01ea44be2f8909e68b031e36d8cc25fd
Esser <upwordstudent@gmail.com>
2021-05-02 12:43:23 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
+# Color info
+BLUE='\033[1;34m'
+LIGHT_GREEN='\033[1;32m'
+NoColor='\033[0m'
+
 # Panel
-
-
+echo "${LIGHT_GREEN}" Updating panel
+https://raw.githubusercontent.com/EsserGaming/PteroUpdater/main/panel.sh
 
 # Wings
+echo "${BLUE}" Updating wings
+https://raw.githubusercontent.com/EsserGaming/PteroUpdater/main/wings.sh
