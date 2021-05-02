819cdafd3b7b8d19c6d0aec2812414a439e4b2a9
Esser <36900138+EsserGaming@users.noreply.github.com>
2021-05-02 13:14:20 -0500

    remove apt update/upgrade

--- wings.sh
+++ wings.sh
@@ -1,6 +1,4 @@
-apt update
-apt upgrade -y
-
+#Update wings
 cd /usr/local/bin
 curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
 chmod u+x /usr/local/bin/wings
